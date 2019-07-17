545ef102c54dc4edf845bc3206899a508df08dd4
Richu Jain <richujain1995@gmail.com>
2019-07-17 18:41:45 -0400

    Initial Commit

---
+++ WebViewiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewiOS
+//
+//  Created by Ashish on 2019-07-17.
+//  Copyright © 2019 Richu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
